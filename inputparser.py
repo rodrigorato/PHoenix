1c79d16d51ffc1049029ac01b043f8b9f19ce288
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2017-11-18 18:53:53 +0000

    All files allowed now

--- inputparser.py
+++ inputparser.py
@@ -1,5 +1,4 @@
 from argparse import ArgumentParser
-import os
 
 
 def validate_input(file_name):
@@ -7,21 +6,11 @@ def validate_input(file_name):
     return file_name
 
 
-def is_valid_file(parser, file_path):
-    input_dir_path = './input/'
-    file_path = validate_input(file_path)
-    if os.path.isfile(file_path) in os.scandir(input_dir_path):
-        parser.error("File {} does not exist.".format(file_path))
-    else:
-        return input_dir_path + file_path
-
-
 def get_input(argv):
     parser = ArgumentParser(description='Analyses PHP code slices for vulnerabilities.')
     parser.add_argument(dest="filename",
                         help="input file name with json PHP code",
-                        metavar="FILE",
-                        type=lambda x: is_valid_file(parser, x))
+                        metavar="FILE")
     # add more options, if needed
     args = parser.parse_args(argv)
     return args.filename
